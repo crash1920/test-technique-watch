eba88d334c6ef08ed6cdd8a3fd8db6925977856e
nader chrigui <44005621+crash1920@users.noreply.github.com>
2023-10-05 10:24:00 +0200

    finishing the clock test

--- watch-healthcare/src/index.tsx
+++ watch-healthcare/src/index.tsx
@@ -1,14 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
 
 import "./index.css";
 import App from "./App";
-import { store } from "./store/store";
 
 ReactDOM.render(
-	<Provider store={store}>
+	<div>
 		<App />
-	</Provider>,
+	</div>,
+
 	document.getElementById("root")
 );
